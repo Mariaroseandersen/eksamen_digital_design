0b4dc91c3130bed066ee48382d9ec5b8147401c1
Camilla <caru0001@stud.kea.dk>
2024-12-19 11:31:03 +0100

    updated config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -14,7 +14,6 @@ export default {
       fontSize: {
         custom54: ["3.375rem", { lineHeight: "1.15" }], // 54px
         custom40: ["2.5rem"], // 40px
-        custom32: ["2rem"], // 32px
       },
 
       /* Corner Radius */
@@ -28,58 +27,22 @@ export default {
         current: "currentColor",
         BarkingAtTheMoon: {
           opacity50: "#E2EBF4",
-          50: "#E2EBF4",
           100: "#C6D6EA",
-          200: "#89ABD3",
-          300: "#5283BD",
           400: "#35577F",
           500: "#1C2E44",
-          600: "#162435",
-          700: "#111C29",
-          800: "#0B121A",
-          900: "#060A0F",
-          950: "#030507",
         },
 
         GoldenRetriever: {
           100: "#FEFAF6",
           200: "#FCF3E6",
-          300: "#FBEEDD",
-          400: "#FAE9D3",
           500: "#F9E3C8",
-          600: "#F0B670",
-          700: "#E68A00",
-          800: "#A05B00",
-          900: "#522F01",
-          950: "#291801",
-        },
-
-        FluffyCloud: {
-          50: "#FFFFFF",
-          100: "#FFFFFF",
-          200: "#FEFBFB",
-          300: "#FEFBFB",
-          400: "#FCF8F6",
-          500: "#FCF6F4",
-          600: "#E9B19F",
-          700: "#D76C4A",
-          800: "#A13B18",
-          900: "#521E0D",
-          950: "#281007",
         },
 
         SalmonTreat: {
-          50: "#FDF0F1",
-          100: "#FBE1E2",
           200: "#F9C9CB",
           300: "#F6ABAD",
-          400: "#F38D90",
           500: "#F17275",
           600: "#ED262B",
-          700: "#CB0000",
-          800: "#890102",
-          900: "#420103",
-          950: "#200102",
         },
       },
 
@@ -93,9 +56,6 @@ export default {
       },
     },
 
-    boxShadow: {
-      dropshadow: "0 4px 4px rgba(89, 89, 89, 0.25)",
-    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
 };
